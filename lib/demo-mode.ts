67fdbd0642ad7cc2be3803249ca0d6068f899def
Jay Callicott <jrcallicott@gmail.com>
2026-03-04 13:55:25 -0600

    Update decoupled-components starter

--- lib/demo-mode.ts
+++ lib/demo-mode.ts
@@ -18,7 +18,7 @@ import type { LandingPage } from './types'
  * Check if demo mode is enabled via environment variable
  */
 export function isDemoMode(): boolean {
-  return process.env.NEXT_PUBLIC_DEMO_MODE === 'true'
+  return process.env.NEXT_PUBLIC_DEMO_MODE !== 'false'
 }
 
 /**
